3ceb5b18f70a75599403231295217eba118ceec8
Shichao Sun <2200011087@stu.pku.edu.cn>
2024-08-09 09:34:45 +0800

    Feat: add Uoc support

--- particle/particle_solver.cpp
+++ particle/particle_solver.cpp
@@ -4,16 +4,51 @@
 #include <eigen3/Eigen/src/Core/ArithmeticSequence.h>
 #include <iostream>
 
+const double delta_u = 0.04;
+const double max_c = 28740.0;
+
 double clamp(double x, double lower, double upper) {
     return x < lower ? lower : (x > upper ? upper : x);
 }
 
+double ueq(double c) {
+    return 0.194 + 1.5 * std::exp(-120.0 * c)
+        + 0.0351 * std::tanh((c - 0.286) / 0.083)
+        - 0.0045 * std::tanh((c - 0.849) / 0.119)
+        - 0.035 * std::tanh((c - 0.9233) / 0.05)
+        - 0.0147 * std::tanh((c - 0.5) / 0.034)
+        - 0.102 * std::tanh((c - 0.194) / 0.142)
+        - 0.022 * std::tanh((c - 0.9) / 0.0164)
+        - 0.011 * std::tanh((c - 0.124) / 0.0226)
+        + 0.0155 * std::tanh((c - 0.105) / 0.029);
+}
+
+double d_ueq(double c) {
+    return -1.5 * (120.0 / max_c) * std::exp(-120.0 * c)
+        + (0.0351 / (0.083 * max_c)) * (1.0 / (std::cosh((c - 0.286) / 0.083) * std::cosh((c - 0.286) / 0.083)))
+        - (0.0045 / (0.119 * max_c)) * (1.0 / (std::cosh((c - 0.849) / 0.119) * std::cosh((c - 0.849) / 0.119)))
+        - (0.035 / (0.05 * max_c)) * (1.0 / (std::cosh((c - 0.9233) / 0.05) * std::cosh((c - 0.9233) / 0.05)))
+        - (0.0147 / (0.034 * max_c)) * (1.0 / (std::cosh((c - 0.5) / 0.034) * std::cosh((c - 0.5) / 0.034)))
+        - (0.102 / (0.142 * max_c)) * (1.0 / (std::cosh((c - 0.194) / 0.142) * std::cosh((c - 0.194) / 0.142)))
+        - (0.022 / (0.0164 * max_c)) * (1.0 / (std::cosh((c - 0.9) / 0.0164) * std::cosh((c - 0.9) / 0.0164)))
+        - (0.011 / (0.0226 * max_c)) * (1.0 / (std::cosh((c - 0.124) / 0.0226) * std::cosh((c - 0.124) / 0.0226)))
+        + (0.0155 / (0.029 * max_c)) * (1.0 / (std::cosh((c - 0.105) / 0.029) * std::cosh((c - 0.105) / 0.029)));
+}
+
+double dc_dx(double c) {
+    return std::sqrt(c * (1.0 - c)) * 0.3478;
+}
+
+double dc_dxdc(double c) {
+    return ((1.0 - 2.0 * c) / (2 * std::sqrt(c * (1.0 - c)))) * 0.3478;
+}
+
 double j_border(double c) {
-    return std::sqrt(c * (1.0 - c)) * 0.3478 * 0.04;
+    return dc_dx(c) * std::sinh(delta_u - ueq(c));
 }
 
 double d_j_border(double c) {
-    return ((1.0 - 2.0 * c) / (2 * std::sqrt(c * (1.0 - c)))) * 0.3478 * 0.04;
+    return dc_dxdc(c) * ueq(c) - dc_dx(c) * d_ueq(c) * std::cosh(delta_u - ueq(c));
 }
 
 void particle_solver::calc(Eigen::Ref<MatrixXd> u) {
